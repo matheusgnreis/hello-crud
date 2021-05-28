ee727c350a2496a3691c04db803dbb7696449161
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2021-05-27 21:55:23 -0300

    chore: get, post, put and delete user

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 app.use(express.json())
 
 const usuarioMatheus = {
-    id: 1,
+    id: 0,
     nome: "Matheus",
     idade: 26
 }
@@ -20,6 +20,38 @@ app.get('/usuarios', (req, res) => {
     res.json(usuarios)
 })
 
+app.post('/usuarios', (req, res) => {
+    const { nome, idade } = req.body
+
+    const id = usuarios.length
+    
+    const novoUsuario = {
+        id,
+        nome,
+        idade
+    }
+
+    usuarios.push(novoUsuario)
+    res.status(201).json(novoUsuario)
+})
+
+app.put('/usuarios/:id', (req, res) => {
+    const { id } = req.params
+    const { nome, idade } = req.body
+    const usuario = usuarios.find(usuario => usuario.id === Number(id))
+    usuario.nome = nome
+    usuario.idade = idade
+
+    res.json(usuario)
+})
+
+app.delete('/usuarios/:id', (req, res) => {
+    const { id } = req.params
+    const index = usuarios.findIndex(usuario => usuario.id === Number(id))
+    usuarios.splice(index, 1)
+
+    res.status(204).send()
+})
 app.listen(port, () => {
     console.log(`Servidor está pronto e rodando na porta ${port}`)
 })
